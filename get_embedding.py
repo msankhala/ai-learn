aea236ba62fc36301b401aa867d2d39e43b2a813
Mahesh Sankhala <maheshsainni@gmail.com>
2025-04-14 23:07:11 +0530

    Initia commit

---
+++ get_embedding.py
@@ -0,0 +1,24 @@
+import openai
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+
+
+# def get_embedding(text):
+#     return openai.Embedding.create(
+#         input=[text.replace("\n", " ")], model="text-embedding-ada-002"
+#     )["data"][0]["embedding"]
+
+
+def get_embedding(text):
+    client = openai.OpenAI()
+    response = client.embeddings.create(
+        input=[text.replace("\n", " ")], model="text-embedding-ada-002"
+    )
+    return response.data[0].embedding
+
+
+empbedding = get_embedding("cat")
+pprint(empbedding)
+print(len(empbedding))
